9671b5e7588c40dadcb6f8a17ea0755c3ddde074
HenryBlackie <HenryBlackie@users.noreply.github.com>
2022-11-20 20:32:18 +0000

    Update 4chan.py
    Added Show Catalog command (-sC)

--- 4chan.py
+++ 4chan.py
@@ -4,6 +4,7 @@ import json
 import os
 import requests
 import sys
+import re
 
 
 def line_break():
@@ -130,7 +131,77 @@ def show_board(board_id, api_endpoints):
 
 
 def show_catalog(board_id, api_endpoints):
-    json_response = requests.get(api_endpoints['catalog']).json()
+    request_url = re.sub('BOARD', board_id, api_endpoints['catalog'])
+    json_response = requests.get(request_url).json()
+
+    # Define catalog attributes and descriptions
+    catalog_attributes = {'no': 'Numeric post ID',
+    'resto': 'ID of parent thread',
+    'sticky': 'Is thread pinned',
+    'closed': 'Is thread closed',
+    'now': 'Creation time in EST/EDT timezone',
+    'time': 'Creation time (UNIX)',
+    'name': 'Name user posted with',
+    'trip': 'User tripcode',
+    'id': 'Poster ID',
+    'capcode': 'Capcode identifier',
+    'country': 'Poster country code (ISO 3166-2 alpha-2)',
+    'country_name': 'Country name',
+    'sub': 'Subject text',
+    'com': 'Comment',
+    'tim': 'Image upload time (UNIX + microtime)',
+    'filename': 'Filename from device',
+    'ext': 'Filetype',
+    'fsize': 'Attachment size (bytes)',
+    'md5': 'Filehash (MD5)',
+    'w': 'Image width',
+    'h': 'Image height',
+    'tn_w': 'Thumbnail width',
+    'tn_h': 'Thumbnail height',
+    'filedeleted': 'Is file deleted',
+    'spoiler': 'Is image spoilered',
+    'custom_spoiler': 'Custom spoiler ID',
+    'omitted_posts': 'Total replies minus previewed replies',
+    'omitted_images': 'Total images minus previewed images',
+    'replies': 'Total replies',
+    'images': 'Total images',
+    'bumplimit': 'Bumplimit reached',
+    'imagelimit': 'Image limit reached',
+    'last_modified': 'Thread last modified (UNIX)',
+    'tag' : '.swf upload category',
+    'semantic_url': 'SEO URL slug',
+    'since4pass': 'Year 4chan pass purchased',
+    'unique_ips': 'Total unique posters',
+    'm_img': 'Mobile optimised image exists',
+    'last_replies': 'JSON representation of most recent replies to thread'
+    }
+
+    # Print catalog information
+    print(f'--- {board_id} Catalog ---')
+    for page in json_response:
+        print(f'-- Page {page["page"]} --')
+        for thread in page['threads']:
+            for k, v in thread.items():
+                # Modify values depending on var type
+                if type(v) == str:
+                    # Unescape HTML characters in strings
+                    v = html.unescape(v)
+                    # Indent comment text
+                    if k == 'com':
+                        v = '\n\t' + '\t'.join(v.splitlines(True))
+                elif k == 'last_replies':
+                    # Parse dictionaries into readable format
+                    v = dict(v[0])
+                    v_list = [f'\t{v_k}: {v_v}\n' for v_k, v_v in v.items()]
+                    v = '\n\t' + ''.join(v_list).strip()
+
+
+                # Print thread details to console
+                # Use attribute description when possible
+                try:
+                    print(f'{catalog_attributes[k]}: {v}')
+                except:
+                    print(f'{k}: {v}')
 
 
 def show_thread(thread_id, api_endpoints):
@@ -145,7 +216,7 @@ def main():
     api_endpoints = {
         'archive': 'https://a.4cdn.org/BOARD/archive.json',
         'boards': 'https://a.4cdn.org/boards.json',
-        'catalog': 'https://a.4cdn.org/BOARD/archive.json'
+        'catalog': 'https://a.4cdn.org/BOARD/catalog.json'
     }
 
     # Call function for appropriate action
